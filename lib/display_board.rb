391fae808d1dd323e84f484c498e86c035e6f939
Cody Fussell <cody.fussell@gmail.com>
2019-08-09 20:05:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,3 +4,6 @@ board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
 def display_board(board)
   puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts 
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
